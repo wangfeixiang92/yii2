3958c3388fe1d693703e19d6b30b313b02e2511b
wangfeixiang92 <2579552905@qq.com>
2018-10-20 12:10:51 +0800

    第一次表单提交（补i充）

--- views/first-from/entry.php
+++ views/first-from/entry.php
@@ -14,12 +14,17 @@ use yii\widgets\ActiveForm;
     在这两个方法之间使用了 field() 方法去创建输入框。
     第一个输入框用于 “name”，第二个输入框用于 “email”。
     之后使用 yii\helpers\Html::submitButton() 方法生成提交按钮。
+    输入框的文字标签是 field() 方法生成的，内容就是模型中该数据的属性名。 例如模型中的 name 属性生成的标签就是 Name。
+
+    你可以在视图中自定义->label标签 按如下方法：
+
+    信息： Yii 提供了相当多类似的小部件去帮你生成复杂且动态的视图。 在后面你还会了解到自己写小部件是多么简单。 你可能会把自己的很多视图代码转化成小部件以提高重用，加快开发效率。
 -->
 <?php $form = ActiveForm::begin();?>
 
-<?= $form->field($model,'name')?>
+<?= $form->field($model,'name')->label('姓名')?>
 
-<?= $form->field($model,'email')?>
+<?= $form->field($model,'email')->label('邮箱')?>
 
 <div class="form-group">
     <?=  Html::submitButton('submit',['btn btn-primary'])?>
