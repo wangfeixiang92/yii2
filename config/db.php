54371cf58b47bc65b17e807852b1f81979bc5ce2
wangfeixiang92 <2579552905@qq.com>
2018-10-20 12:57:26 +0800

    新增vendor扩展库

--- config/db.php
+++ config/db.php
@@ -1,10 +1,13 @@
 <?php
-
+/*
+ *  这里是数据库的配置信息
+ *
+ * */
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=192.168.33.10;dbname=yii2basic',
+    'username' => 'homestead',
+    'password' => 'homestead',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
